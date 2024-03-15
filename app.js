8008bae77fd0a91c47e4404a511b5e9e524f3f62
xiaofan10 <xiaoruiaini88@163.com>
2024-03-15 10:47:33 +0800

    ci: 调整配置

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const helmet = require('helmet')
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const helmet = require("helmet");
+const os = require("os");
+
 // const compression = require('compression');
 
-const routeMiddleware = require('./routes/index');
-const ws = require('./utils/ws');
+const routeMiddleware = require("./routes/index");
+const ws = require("./utils/ws");
 
 const app = express();
 
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || '3000');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || "3000");
 
 const sesisonMiddleware = session({
-  secret: 'llwb', 
-  cookie: ({ path: '/', httpOnly: true, secure: false, maxAge: null }),
-  resave: true,  
-  saveUninitialized: true
+  secret: "llwb",
+  cookie: { path: "/", httpOnly: true, secure: false, maxAge: null },
+  resave: true,
+  saveUninitialized: true,
 });
 
 // 如果使用Nginx，则在nginx处理gzip即可
@@ -34,41 +36,62 @@ app.use(helmet());
 // session 中间件
 app.use(sesisonMiddleware);
 // 日志中间件
-app.use(logger('dev'));
+app.use(logger("dev"));
 // parse application/json，express@4.16.0内置，替代了 body-parser
 app.use(express.json());
 // parse application/x-www-form-urlencoded，替代了 body-parser
 app.use(express.urlencoded({ extended: false }));
 // 解析得到 req.cookies
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // 路由中间件
 routeMiddleware(app);
 
-server.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+// 获取本地 IP 地址
+function getLocalIpAddress() {
+  const ifaces = os.networkInterfaces();
+  for (const iface in ifaces) {
+    for (const info of ifaces[iface]) {
+      if (info.family === "IPv4" && !info.internal) {
+        return info.address;
+      }
+    }
+  }
+  return "127.0.0.1"; // 默认返回本地回环地址
+}
+
+server.listen(app.get("port"), getLocalIpAddress(), function () {
+  const address = server.address();
+  console.log(address);
+  console.log(
+    "Express server listening on port http://" +
+      address.address +
+      ":" +
+      address.port
+  );
 });
 
 // socket io
 ws.init(io);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+
+  res.render("error");
 });
 
 module.exports = app;
--- utils/db.js
+++ utils/db.js
@@ -7,16 +7,16 @@
  */
 const mysql = require("mysql");
 const config = require("../config");
-const errcode = require('../utils/errcode');
+const errcode = require("../utils/errcode");
 
 const pool = mysql.createPool(config.mysql);
 
-pool.on('connection', (connection) => {
-    // console.log('取得连接');
+pool.on("connection", (connection) => {
+  console.log("取得连接");
 });
 
-pool.on('release', (connection) => {
-    // console.log('释放了连接');
+pool.on("release", (connection) => {
+  console.log("释放了连接");
 });
 
 pool.on("error", (err) => {
@@ -24,45 +24,45 @@ pool.on("error", (err) => {
 });
 
 function getConnection(res) {
-    return new Promise((resolve, reject) => {
-        pool.getConnection(function(err, connection) {
-            if (err) {
-                // not connected!
-                console.error(err);
-                if (res) {
-                    res.send({
-                        ...errcode.AUTH.UNAUTHORIZED
-                    });
-                }
-                reject(err)
-            } else {
-                resolve(connection)
-            }
-        })
-    })  
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        // not connected!
+        console.error(err);
+        if (res) {
+          res.send({
+            ...errcode.AUTH.UNAUTHORIZED,
+          });
+        }
+        reject(err);
+      } else {
+        resolve(connection);
+      }
+    });
+  });
 }
 
 function execQuery(options, connection, release = true) {
-    return new Promise((resolve, reject) => {
-        // Use the connection
-        connection.query(options, function (error, results, fields) {
-            // When done with the connection, release it.
-            if (release) {
-                // 可以手动选择不 release，复用 connection，在事务结束再 release
-                connection.release();
-            }
+  return new Promise((resolve, reject) => {
+    // Use the connection
+    connection.query(options, function (error, results, fields) {
+      // When done with the connection, release it.
+      if (release) {
+        // 可以手动选择不 release，复用 connection，在事务结束再 release
+        connection.release();
+      }
 
-            if (error) {
-                // Handle error after the release.
-                reject(error)
-            } else {
-                resolve({
-                    results,
-                    fields
-                })
-            }
+      if (error) {
+        // Handle error after the release.
+        reject(error);
+      } else {
+        resolve({
+          results,
+          fields,
         });
-    })
+      }
+    });
+  });
 }
 
 /**
@@ -72,45 +72,49 @@ function execQuery(options, connection, release = true) {
  * @param {boolean} release 执行 query 后，是否自动 release
  */
 function query(options, connection, release) {
-    if (connection) {
-        return execQuery(options, connection, release)
-    } else {
-        return getConnection().then(connection => {
-            return execQuery(options, connection, release)
-        })
-    }
-    
+  if (connection) {
+    return execQuery(options, connection, release);
+  } else {
+    return getConnection().then((connection) => {
+      return execQuery(options, connection, release);
+    });
+  }
 }
 
 function execTransaction(connection, task) {
-    return new Promise((resolve, reject) => {
-        connection.beginTransaction(function(err) {
-            if (err) {
-                reject(err)
-            }
-            task.then(resp => {
-                connection.commit(function(err) {
-                    if (err) {
-                        connection.rollback(function() {
-                            reject(err)
-                        });
-                    } else {
-                        resolve(resp)
-                    }
+  return new Promise((resolve, reject) => {
+    connection.beginTransaction(function (err) {
+      if (err) {
+        reject(err);
+      }
+      task
+        .then(
+          (resp) => {
+            connection.commit(function (err) {
+              if (err) {
+                connection.rollback(function () {
+                  reject(err);
                 });
-            }, err => {
-                connection.rollback(function() {
-                    reject(err)
-                });
-            }).catch(err => {
-                connection.rollback(function() {
-                    reject(err)
-                });
-            })
+              } else {
+                resolve(resp);
+              }
+            });
+          },
+          (err) => {
+            connection.rollback(function () {
+              reject(err);
+            });
+          }
+        )
+        .catch((err) => {
+          connection.rollback(function () {
+            reject(err);
+          });
         });
-    }).finally(() => {
-        connection.release();
-    })
+    });
+  }).finally(() => {
+    connection.release();
+  });
 }
 
 module.exports.getConnection = getConnection;
